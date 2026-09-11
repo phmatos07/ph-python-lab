c98a337285decc5f3dc1f63ced4d1820c6a84fbf
phmatos07 <phelipe.matos07@gmail.com>
2026-09-10 22:47:56 -0300

    feat: Implement initial variable definitions and type printing
    Author: Phelipe Matos (PHMATOS07)

---
+++ src/type.py
@@ -0,0 +1,11 @@
+name = "PH"
+age = 38
+height = 1.73
+developer = True
+nickname = None
+
+print("Valor de name:", name, "| Tipo:", type(name))
+print("Valor de age:", age, "| Tipo:", type(age))
+print("Valor de height:", height, "| Tipo:", type(height))
+print("Valor de developer:", developer, "| Tipo:", type(developer))
+print("Valor de nickname:", nickname, "| Tipo:", type(nickname))
